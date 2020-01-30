35318031e346f7535e8d21336ab30818e400b07d
Kevin Mittal <33699729+kevmittal@users.noreply.github.com>
2020-01-30 23:32:58 +0530

    Add files via upload

--- background.js
+++ background.js
@@ -1,54 +1,34 @@
-/* global chrome */
+/* global chrome, alert */
 
 var selector = 'document.querySelector(\'link[rel="amphtml"]\')'
 
-function active (amp) {
-  chrome.browserAction.setIcon({ path: 'icons/active.png' })
-  chrome.browserAction.setTitle({ title: 'Click to open ' + (amp ? 'canonical' : 'Superchargify AMP') + ' page in a new tab' })
+function createAlert () {
+  alert('This page doesn\'t have a Superchargify AMP page')
 }
 
-function inactive () {
-  chrome.browserAction.setIcon({ path: 'icons/inactive.png' })
-  chrome.browserAction.setTitle({ title: 'Unable to find Superchargify AMP on this page' })
-}
+function handleClick (tab) {
+  var url = tab.url
 
-function run (click) {
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    var url = tabs[0].url
+  if (url.indexOf('/a/sc/amp/') !== -1) {
+    chrome.tabs.create({ url: url.replace('/a/sc/amp', '') })
+    return
+  }
 
-    if (url.indexOf('/a/sc/amp/') !== -1) {
-      if (click) {
-        chrome.tabs.create({ url: url.replace('/a/sc/amp', '') })
-        return
-      }
+  chrome.tabs.executeScript(
+    {
+      code: selector + ' && ' + selector + '.href',
+      runAt: 'document_end'
+    },
+    function (values) {
+      var url = values && values[0]
+      if (!url) return createAlert()
 
-      active(true)
-      return
+      var isSuperchargifyAMP = url.indexOf('/a/sc/amp/') !== -1
+      if (!isSuperchargifyAMP) return createAlert()
+
+      chrome.tabs.create({ url })
     }
-
-    chrome.tabs.executeScript(
-      {
-        code: selector + ' && ' + selector + '.href',
-        runAt: 'document_end'
-      },
-      function (values) {
-        var url = values && values[0]
-        if (!url) return inactive()
-
-        var isSuperchargifyAMP = url.indexOf('/a/sc/amp/') !== -1
-        if (!isSuperchargifyAMP) return inactive()
-
-        if (click) {
-          chrome.tabs.create({ url })
-          return
-        }
-
-        active()
-      }
-    )
-  })
+  )
 }
 
-chrome.tabs.onUpdated.addListener(function () { run() })
-chrome.tabs.onActivated.addListener(function () { run() })
-chrome.browserAction.onClicked.addListener(function () { run(true) })
+chrome.browserAction.onClicked.addListener(handleClick)
